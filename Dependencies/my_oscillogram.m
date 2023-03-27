4e31654a0fd63bb8538ffb6609098ce2cc3048b1
rernamp <renamp100@gmail.com>
2023-03-27 22:11:26 +0300

    change my_oscillogram.m for plot iq signal

--- Dependencies/my_oscillogram.m
+++ Dependencies/my_oscillogram.m
@@ -4,12 +4,8 @@ len = length(IQ_mas);
 t_mas = (0:1:len-1)/SAMPLE_RATE_Hz*1000;
 figure();
 hold all;
-plot(t_mas, real(IQ_mas));
-plot(t_mas, imag(IQ_mas));
-plot(t_mas, abs(IQ_mas));
+plot(IQ_mas);
 hold off;
-xlabel('t, мсек');
-title('Осциллограмма');
 grid on;
 
 no = 1;
