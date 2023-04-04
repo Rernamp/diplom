bc85795ca7464a0529375a275adb4c0fda3eaff4
Vratarishka <tupitsin120200@gmail.com>
2023-04-04 21:37:59 +0300

    %

--- ExampleForNet/TestLLR.m
+++ ExampleForNet/TestLLR.m
@@ -12,7 +12,7 @@ else
     load('llrnetDVBS2Networks','llrNets','subsystemType','EsNoValues');
 end
 
-subsystemType = '8PSK 2/3'; %#ok<UNRCH>
+subsystemType = '16APSK 2/3'; %#ok<UNRCH>
 EsNoValues = 6:0.1:6.7;     % in dB
 
 estimateConfig = LLREstimateConfig(false, @(input) (amplifaerSaleh(input)));
@@ -22,12 +22,12 @@ estimateConfig = LLREstimateConfig(false, @(input) (amplifaerSaleh(input)));
 type = subsystemType + " + Saleh";
 llrnetPlotLLRvsEsNo(perLLRSalef,perApproxLLRSalef,perLLRNetSalef, EsNoValues,type)
 
-estimateConfig = LLREstimateConfig(false, @(input) (amplifaerGhorbani(input)));
+%%estimateConfig = LLREstimateConfig(false, @(input) (amplifaerGhorbani(input)));
 
 % Simulate PER with exact LLR, approximate LLR, and LLRNet
-[perLLRGhorbani,perApproxLLRGhorbani,perLLRNetGhorbani] = customLlrnetDVBS2PER(subsystemType,EsNoValues,llrNets,numFrames,numErrors, estimateConfig);
-type = subsystemType + " + Ghorbani";
-llrnetPlotLLRvsEsNo(perLLRGhorbani,perApproxLLRGhorbani,perLLRNetGhorbani,EsNoValues,type)
+%[perLLRGhorbani,perApproxLLRGhorbani,perLLRNetGhorbani] = customLlrnetDVBS2PER(subsystemType,EsNoValues,llrNets,numFrames,numErrors, estimateConfig);
+%type = subsystemType + " + Ghorbani";
+%llrnetPlotLLRvsEsNo(perLLRGhorbani,perApproxLLRGhorbani,perLLRNetGhorbani,EsNoValues,type)
 
 estimateConfig = LLREstimateConfig(false, @(input) (amplifaerDummy(input)));
 
@@ -48,37 +48,37 @@ ylabel('PER')
 grid on
 legend('Exact LLR', 'Exact Saleh LLR','Approx. LLR', 'Approx. Saleh LLR')
 
-figure ()
-semilogy(EsNoValues, perLLRDummy(:,1))
-hold on
-semilogy(EsNoValues, perLLRGhorbani(:,1))
-semilogy(EsNoValues, perApproxLLRDummy(:,1))
-semilogy(EsNoValues, perApproxLLRGhorbani(:,1))
-xlabel('E_s/N_o (dB)')
-ylabel('PER')
-grid on
-legend('Exact LLR', 'Exact Ghorbani LLR','Approx. LLR', 'Approx. Ghorbani LLR')
+% figure ()
+% semilogy(EsNoValues, perLLRDummy(:,1))
+% hold on
+% semilogy(EsNoValues, perLLRGhorbani(:,1))
+% semilogy(EsNoValues, perApproxLLRDummy(:,1))
+% semilogy(EsNoValues, perApproxLLRGhorbani(:,1))
+% xlabel('E_s/N_o (dB)')
+% ylabel('PER')
+% grid on
+% legend('Exact LLR', 'Exact Ghorbani LLR','Approx. LLR', 'Approx. Ghorbani LLR')
+% 
+% function output = amplifaerGhorbani(input)
 
-function output = amplifaerGhorbani(input)
+% x_1 = 1.92;
+% x_2 = 1.74;
+% x_3 = 0.92;
+% x_4 = 0;
+% x_5 = 1.74;
+% y_1 = 0.02;
+% y_2 = 1;
+% y_3 = 0.4;
+% y_4 = 0;
+% y_5 = 3.5;
+% 
+% G = (x_1.*abs(xComplex).^x_2)./(1+x_3.*abs(xComplex).^x_5)+x_4.*abs(xComplex);
+% F = (y_1.*abs(xComplex).^y_2)./(1+y_3.*abs(xComplex).^y_5)+y_4.*abs(xComplex).^y_2;
+% 
+% output = G .* exp(1i * 2 * pi .* F);
+% output = input .* (output ./ abs(input));
 
-x_1 = 1.92;
-x_2 = 1.74;
-x_3 = 0.92;
-x_4 = 0;
-x_5 = 1.74;
-y_1 = 0.02;
-y_2 = 1;
-y_3 = 0.4;
-y_4 = 0;
-y_5 = 3.5;
-
-G = (x_1.*abs(xComplex).^x_2)./(1+x_3.*abs(xComplex).^x_5)+x_4.*abs(xComplex);
-F = (y_1.*abs(xComplex).^y_2)./(1+y_3.*abs(xComplex).^y_5)+y_4.*abs(xComplex).^y_2;
-
-output = G .* exp(1i * 2 * pi .* F);
-output = input .* (output ./ abs(input));
-
-end
+% end
 
 
 function output = amplifaerSaleh(input)
